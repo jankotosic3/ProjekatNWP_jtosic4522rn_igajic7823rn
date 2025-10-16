8604a368e8de7601380e076ea640c397f6e6864a
IgorGajic <igajic7823rn@raf.rs>
2025-10-16 02:50:34 +0200

    dodao sam Home component na kojoj samo pise Home, da ne bi bila prazna ona prva strana, jako me je nerviralo

--- raf-cloud-frontend/src/app/app.routes.ts
+++ raf-cloud-frontend/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserTableComponent } from './user-table/user-table.component';
 import { AddNewUserComponent } from './add-new-user/add-new-user.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent},
   { path: 'user-login', component: UserLoginComponent },
   { path: 'user-table', component: UserTableComponent },
   { path: 'new-user', component: AddNewUserComponent}
