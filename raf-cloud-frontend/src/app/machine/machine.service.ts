6bed0d96a4c6b97708807adc50d790c8741a76de
IgorGajic <igajic7823rn@raf.rs>
2025-10-30 22:50:05 +0100

    radi search po imenu masine i da li je aktivna ili ne

--- raf-cloud-frontend/src/app/machine/machine.service.ts
+++ raf-cloud-frontend/src/app/machine/machine.service.ts
@@ -1,68 +1,85 @@
-import { Injectable } from "@angular/core";
-import { DUMMY_MACHINES } from "../dummy-machines";
-import { type Machine } from "./machine.model";
-import { AuthService } from "../auth.service";
-import {Router} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { DUMMY_MACHINES } from '../dummy-machines';
+import { MachineStatus, type Machine } from './machine.model';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class MachineService { 
-    machines: Machine[] = DUMMY_MACHINES;
-    constructor(private router: Router,private authService: AuthService) {}
+export class MachineService {
+  machines: Machine[] = DUMMY_MACHINES;
+  constructor(private router: Router, private authService: AuthService) {}
 
-    search(){
-        
-    }
-    turnOn(){
-        //TO DO
-    }
-    turnOff(){
-        //TO DO
-    }
-    restart(){ //valjda ovako (nisam citao al pretpostavljam)
-        this.turnOff();
-        this.turnOn();
-    }
+  search(name?: string, status?: MachineStatus | null): Machine[] {
+    return this.getMachinesForLoggedInUser().filter((machine) => {
+      if (
+        name &&
+        !machine.machineName.toLowerCase().includes(name.toLowerCase())
+      )
+        return false;
 
-    createMachine(machineName: string){
-        this.machines.push({
-            id: this.machines.length,
-            machineName: machineName,
-            status: 0,
-            creatorId: this.authService.userLoggedIn?.id || '',
-            active: false
-        });
+      if (status !== null && status !== undefined && machine.status !== status)
+        return false;
 
-        this.router.navigate(['/machines']);
-    }
-    deleteMachine(machineId: number){
-        for(let machine of this.machines){
-            if(machine.id === machineId){
-                if(machine.active){
-                    return;
-                }
-            }
+      return true;
+    });
+  }
+
+  turnOn() {
+    //TO DO
+  }
+  turnOff() {
+    //TO DO
+  }
+  restart() {
+    //valjda ovako (nisam citao al pretpostavljam)
+    this.turnOff();
+    this.turnOn();
+  }
+
+  createMachine(machineName: string) {
+    this.machines.push({
+      id: this.machines.length,
+      machineName: machineName,
+      status: 0,
+      creatorId: this.authService.userLoggedIn?.id || '',
+      active: false,
+    });
+
+    this.router.navigate(['/machines']);
+  }
+  deleteMachine(machineId: number) {
+    for (let machine of this.machines) {
+      if (machine.id === machineId) {
+        if (machine.active) {
+          return;
         }
-        this.machines.filter(machine => machine.id !== machineId);
+      }
     }
+    this.machines.filter((machine) => machine.id !== machineId);
+  }
 
-    getMachinesForLoggedInUser(): Machine[] {
-        if(this.authService.userLoggedIn?.userType === 'admin'){
-            return this.machines;
-        }
-        return this.machines.filter(machine => machine.creatorId === this.authService.userLoggedIn?.id);
+  getMachinesForLoggedInUser(): Machine[] {
+    if (this.authService.userLoggedIn?.userType === 'admin') {
+      return this.machines;
     }
+    return this.machines.filter(
+      (machine) => machine.creatorId === this.authService.userLoggedIn?.id
+    );
+  }
 
-    returnMachinesForLoggedInUser(): Machine[] {
-        return this.machines.filter(machine => machine.creatorId === this.authService.userLoggedIn?.id);
-    }
-    
-    getUserNameForMachine(userId: string): string {
-        return this.authService.getUserNameById(userId);
-    }
+  returnMachinesForLoggedInUser(): Machine[] {
+    return this.machines.filter(
+      (machine) => machine.creatorId === this.authService.userLoggedIn?.id
+    );
+  }
 
-    loggedInUser(){
-        return this.authService.userLoggedIn;
-    }
+  getUserNameForMachine(userId: string): string {
+    return this.authService.getUserNameById(userId);
+  }
+
+  loggedInUser() {
+    return this.authService.userLoggedIn;
+  }
 }
--- raf-cloud-frontend/src/app/machine/search-machines/search-machines.component.ts
+++ raf-cloud-frontend/src/app/machine/search-machines/search-machines.component.ts
@@ -1,26 +1,30 @@
 import { Component } from '@angular/core';
 import { MachineService } from '../machine.service';
-import { MachineStatus } from '../machine.model';
+import { Machine, MachineStatus } from '../machine.model';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-machines',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-machines.component.html',
   styleUrl: './search-machines.component.css',
 })
 export class SearchMachinesComponent {
-  constructor(private router: Router, private machineService: MachineService) {}
+  machines!: Machine[];
+  
+  constructor(private router: Router, private machineService: MachineService) {
+    this.machines = this.machineService.getMachinesForLoggedInUser();
+  }
+
+  searchName?: string;
+  searchStatus: MachineStatus | null = null;
 
   //Primer koliko je frontend glup, ovo mora da bi se enum vrednost bila prikazana kao string a ne kao broj u html-u, djubre klasnicno
   MachineStatus = MachineStatus;
 
-  get machines() {
-    return this.machineService.getMachinesForLoggedInUser();
-  }
-
-  get loggedInUser(){
+  get loggedInUser() {
     return this.machineService.loggedInUser();
   }
 
@@ -30,5 +34,9 @@ export class SearchMachinesComponent {
 
   onNewMachine() {
     this.router.navigate(['/new-machine']);
+  }
+
+  onSearch() {
+    this.machines = this.machineService.search(this.searchName, this.searchStatus);
   }
 }
