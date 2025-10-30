84383a1fa36b3b7c1cfa421105e54eab570aeff6
IgorGajic <igajic7823rn@raf.rs>
2025-10-30 23:38:29 +0100

    poceo da radim na error-table-u, mada me smara isk, zavrsicu sutra

--- raf-cloud-frontend/src/app/machine/machine-error-table/machine-error-table.component.ts
+++ raf-cloud-frontend/src/app/machine/machine-error-table/machine-error-table.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { MachineService } from '../machine.service';
 
 @Component({
   selector: 'app-machine-error-table',
   standalone: true,
   imports: [],
   templateUrl: './machine-error-table.component.html',
-  styleUrl: './machine-error-table.component.css'
+  styleUrl: './machine-error-table.component.css',
 })
 export class MachineErrorTableComponent {
+  constructor(private machineSerivce: MachineService) {}
 
+  getNumberOfUsersMachinePermissions() : number {
+    const user = this.loggedInUser;
+    let result = 0;
+
+    if (user?.createMachinePermission) result++;
+    if (user?.deleteMachinePermission) result++;
+    if (user?.searchMachinePermission) result++;
+    if (user?.turnOnMachinePermission) result++;
+    if (user?.restartMachinePermission) result++;
+    if (user?.turnOffMachinePermission) result++;
+
+    return result;
+  }
+
+  get loggedInUser() {
+    return this.machineSerivce.loggedInUser();
+  }
 }
